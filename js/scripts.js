da1e4e5d49dacbc536bca8ec67bd559b244d7e1f
Sergio <me@sergiom23.com>
2014-05-07 16:48:26 -0500

    Vendor and done

--- js/scripts.js
+++ js/scripts.js
@@ -42,28 +42,4 @@ Slides Background
     })();
 });
 
-/*============================
-Navbar Affix
-============================*/
-
-
-
-/*============================
-Vendor Function scroll with jQuery
-============================*/
-
-    (document).ready(function(){
-    ('a[href^="#"]').on('click',function (e) {
-        e.preventDefault();
-
-        var target = this.hash,
-        target = (target);
-
-        ('html, body').stop().animate({
-            'scrollTop': target.offset().top
-        }, 900, 'swing', function () {
-            window.location.hash = target;
-         });
-       });
-    });
 
