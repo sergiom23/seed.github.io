16b1814cdcc080967289f801c81f3c7f6f147194
Sergio <me@sergiom23.com>
2014-05-07 16:22:53 -0500

    update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,12 @@
-$(document).ready(function () {
+/************
+ Scripts
+*************/
+
+
+/*============================
+Slides Background
+============================*/
+(document).ready(function () {
     var img_array = [0, 1, 2, 3, 4, 5],
         newIndex = 0,
         index = 0,
@@ -18,18 +26,63 @@ $(document).ready(function () {
 
             index = (index + 1) % img_array.length;
 
-        $('.intro-header').css('background').fadeOut('slow',function() {
-            $('.intro-header').animate({
+        ('.intro-header').css('background') {
+            ('.intro-header').animate({
                 backgroundColor: 'transparent'
             }, 1000, function () {
                 setTimeout(function () {
-                    $('.intro-header').animate({
-                        'background-color': 'url(/img/bg0.jpg)'.fadeIn('slow')
+                    ('.intro-header').animate({
+                        'background-color': 'url(/img/bg0.jpg)'
                     }, 1000);
                 }, 3000);
             });
-            return 'url(/img/bg' + img_array[index] + '.jpg)'.fadeIn('slow');
+            return 'url(/img/bg' + img_array[index] + '.jpg)';
         });
         setTimeout(changeBg, interval);
     })();
 });
+
+/*============================
+Navbar Affix
+============================*/
+
+    (function ($) {
+    (document).ready(function(){
+
+    // hide .navbar first
+    (".navbar").hide();
+
+    // fade in .navbar
+    (function () {
+        (window).scroll(function () {
+
+                 // set distance user needs to scroll before we start fadeIn
+            if ((this).scrollTop() > 600) {
+                ('.navbar').fadeIn();
+            } else {
+                ('.navbar').fadeOut();
+            }
+          });
+         });
+       });
+     }(jQuery));
+
+/*============================
+Vendor Function scroll with jQuery
+============================*/
+
+    (document).ready(function(){
+    ('a[href^="#"]').on('click',function (e) {
+        e.preventDefault();
+
+        var target = this.hash,
+        target = (target);
+
+        ('html, body').stop().animate({
+            'scrollTop': target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+         });
+       });
+    });
+
