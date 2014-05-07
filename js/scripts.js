b6d88e4b357bc4e211a2fa13e719ecbe16889ab3
Sergio <me@sergiom23.com>
2014-05-07 15:34:55 -0500

    Color

--- js/scripts.js
+++ js/scripts.js
@@ -24,7 +24,7 @@ $(document).ready(function () {
             }, 1000, function () {
                 setTimeout(function () {
                     $('.intro-header').animate({
-                        backgroundColor: 'rgb(49,181,139)'
+                        'background-color': 'rgb(49,181,139)'
                     }, 1000);
                 }, 3000);
             });
