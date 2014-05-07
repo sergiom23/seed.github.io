54f27b11a3437974e80ba6548fb450aa8dfbce3c
Sergio <me@sergiom23.com>
2014-05-07 16:27:47 -0500

    updated :)

--- js/scripts.js
+++ js/scripts.js
@@ -46,26 +46,7 @@ Slides Background
 Navbar Affix
 ============================*/
 
-    (function ($) {
-    (document).ready(function(){
 
-    // hide .navbar first
-    (".navbar").hide();
-
-    // fade in .navbar
-    (function () {
-        (window).scroll(function () {
-
-                 // set distance user needs to scroll before we start fadeIn
-            if ((this).scrollTop() > 600) {
-                ('.navbar').fadeIn();
-            } else {
-                ('.navbar').fadeOut();
-            }
-          });
-         });
-       });
-     }(jQuery));
 
 /*============================
 Vendor Function scroll with jQuery
